9986f2faf4cf1b679574e17a77b043ed2ec9d64c
reinanbr <perseu912@gmail.com>
2024-11-28 22:08:24 -0300

    upload to vercel try.3

---
+++ api/read_data.go
@@ -0,0 +1,30 @@
+package handler
+
+
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"net/http"
+	"api_save_data/api/db"
+	"api_save_data/api/models"
+)
+
+
+func Read_access(w http.ResponseWriter,r*http.Request){
+	pool :=psql_vercel.ConnectDB()
+        defer pool.Close()
+	infoAccess,err := access_site_model.ReadAccessSites(pool)
+	if err == nil{
+		w.Header().Set("Content-Type", "application/json")
+		if errJson := json.NewEncoder(w).Encode(infoAccess); errJson != nil {
+			http.Error(w, "Erro ao gerar o JSON", http.StatusInternalServerError)
+			log.Printf("Erro ao codificar JSON: %v", errJson)
+	}
+	}else{
+		fmt.Fprintf(w,"error: %v\n",err)
+	}
+}
+

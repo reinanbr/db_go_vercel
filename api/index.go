2e55a8b805c12c76dc95e749957359dfd5c9e0e7
reinanbr <perseu912@gmail.com>
2024-11-29 08:39:57 -0300

    upload to vercel try.23

--- api/index.go
+++ api/index.go
@@ -1,35 +1,22 @@
 package handler
-
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"time"
-	. "github.com/tbxark/g4vercel"
 )
 
+type Response struct {
+	Message   string `json:"message"`
+	Timestamp string `json:"timestamp"`
+}
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New();
-	server.Use(Recovery(func(err interface{}, c *Context) {
-		if httpError, ok := err.(HttpError); ok {
-			c.JSON(httpError.Status, H{
-				"message": httpError.Error(),
-			})
-		} else {
-			message := fmt.Sprintf("%s", err)
-			c.JSON(500, H{
-				"message": message,
-			})
-		}
-	}))
-	server.GET("/",func(context *Context) {
+// Handler é a função exportada para o Vercel.
+func Index(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Message:   "Estamos online",
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
 
-//	response := ResponseIndex{
-//		Message:   "Estamos online",
-//		Timestamp: time.Now().Format(time.RFC3339),
-//	};
-	
-		context.JSON(200,H{"message":"estamos fudendo","date":time.Now().Format(time.RFC3339)});
-	})	
-	server.Handle(w,r);
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }

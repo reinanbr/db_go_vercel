9d9a4e2207226d1f77fbfb1b9e5217e811ec01d1
reinanbr <perseu912@gmail.com>
2024-11-28 21:51:07 -0300

    upload to vercel try.1

---
+++ api/index.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type ResponseIndex struct {
+	Message   string `json:"message"`
+	Timestamp string `json:"timestamp"`
+}
+
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	response := ResponseIndex{
+		Message:   "Estamos online",
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+	fmt.Print("server ok\n")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+

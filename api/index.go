95d3967e4e55a90d976533223f76b63e025916e5
reinanbr <perseu912@gmail.com>
2024-11-29 08:54:58 -0300

    upload to vercel try.26

--- api/index.go
+++ api/index.go
@@ -3,7 +3,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
-        "encoding/json"
         "fmt"
         "log"
         "api_save_data/api/db"
@@ -31,17 +30,18 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 
 
-
 func ReadAccess(w http.ResponseWriter,r*http.Request){
         pool :=psql_vercel.ConnectDB()
         defer pool.Close()
         infoAccess,err := access_site_model.ReadAccessSites(pool)
         if err == nil{
                 w.Header().Set("Content-Type", "application/json")
-                if errJson := json.NewEncoder(w).Encode(infoAccess); errJson != nil {
+		if errJson := json.NewEncoder(w).Encode(infoAccess); errJson != nil {
                         http.Error(w, "Erro ao gerar o JSON", http.StatusInternalServerError)
                         log.Printf("Erro ao codificar JSON: %v", errJson)
         }
         }else{
                 fmt.Fprintf(w,"error: %v\n",err)
-        }                                                                               }
+        }
+}
+
